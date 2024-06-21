bead9e9346f6abe73e1c3cc8d10eedce58659b08
Antoniomjg <antoniomjg1@gmail.com>
2024-06-21 18:39:34 +0200

    Resto metodos crud

--- Domain/Repositories/IRepositoryBase.cs
+++ Domain/Repositories/IRepositoryBase.cs
@@ -2,7 +2,10 @@
 {
     public interface IRepositoryBase<TEntity> where TEntity : class
     {
+        Task<IEnumerable<TEntity>> GetAllAsync<TEntity>() where TEntity : class, IBase;
         Task<TEntity> GetByIdAsync<TEntity>(int id) where TEntity : class, IBase;
         Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(int id);
     }
 }
